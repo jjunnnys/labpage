637156253c662181ec3697b6f8286d05590d2a11
ppby <salswnss@gmail.com>
2020-06-15 16:15:54 +0900

    Finish member

--- public/js/member/member.js
+++ public/js/member/member.js
@@ -2,8 +2,10 @@ const bgWrapper = document.querySelector('.bg-wrapper');
 const bgWrapperText = document.querySelector('.bg-wrapper__text');
 const mImg = document.querySelectorAll('.js-mImg');
 const mInfo = document.querySelectorAll('.member__info');
-const mInfoR = document.querySelectorAll('.member__infoR');
 const email = document.querySelectorAll('.email');
+const github = document.querySelector('.github');
+
+console.log(github);
 
 let last_known_scroll_position = 0;
 let ticking = false;
@@ -19,15 +21,17 @@ const imgHandle = {
   showEmail1: () => email[1].classList.add('email-show'),
   hiddenEmail1: () => email[1].classList.remove('email-show'),
 
-  showImgR0: () => mInfoR[0].classList.add('member__info-showR'),
-  hiddenImgR0: () => mInfoR[0].classList.remove('member__info-showR'),
+  showImg2: () => mInfo[2].classList.add('member__info-show'),
+  hiddenImg2: () => mInfo[2].classList.remove('member__info-show'),
   showEmail2: () => email[2].classList.add('email-show'),
   hiddenEmail2: () => email[2].classList.remove('email-show'),
 
-  showImgR1: () => mInfoR[1].classList.add('member__info-showR'),
-  hiddenImgR1: () => mInfoR[1].classList.remove('member__info-showR'),
+  showImg3: () => mInfo[3].classList.add('member__info-show'),
+  hiddenImg3: () => mInfo[3].classList.remove('member__info-show'),
   showEmail3: () => email[3].classList.add('email-show'),
   hiddenEmail3: () => email[3].classList.remove('email-show'),
+  showGithub: () => github.classList.add('github-show'),
+  hiddenGithub: () => github.classList.remove('github-show'),
 };
 
 const bgText = (scroll_pos) => {
@@ -55,15 +59,17 @@ const Member = () => {
   mImg[1].addEventListener('mouseleave', imgHandle.hiddenImg1);
   mImg[1].addEventListener('mouseleave', imgHandle.hiddenEmail1);
 
-  mImg[2].addEventListener('mouseenter', imgHandle.showImgR0);
+  mImg[2].addEventListener('mouseenter', imgHandle.showImg2);
   mImg[2].addEventListener('mouseenter', imgHandle.showEmail2);
-  mImg[2].addEventListener('mouseleave', imgHandle.hiddenImgR0);
+  mImg[2].addEventListener('mouseleave', imgHandle.hiddenImg2);
   mImg[2].addEventListener('mouseleave', imgHandle.hiddenEmail2);
 
-  mImg[3].addEventListener('mouseenter', imgHandle.showImgR1);
+  mImg[3].addEventListener('mouseenter', imgHandle.showImg3);
   mImg[3].addEventListener('mouseenter', imgHandle.showEmail3);
-  mImg[3].addEventListener('mouseleave', imgHandle.hiddenImgR1);
+  mImg[3].addEventListener('mouseleave', imgHandle.hiddenImg3);
   mImg[3].addEventListener('mouseleave', imgHandle.hiddenEmail3);
+  mImg[3].addEventListener('mouseenter', imgHandle.showGithub);
+  mImg[3].addEventListener('mouseleave', imgHandle.hiddenGithub);
 };
 
 Member();
