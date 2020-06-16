208d1c673bae56c09ef5f5a30465a7f5158d4552
ppby <salswnss@gmail.com>
2020-06-16 13:40:56 +0900

    Add research journal modal

--- public/js/research/research.js
+++ public/js/research/research.js
@@ -1,8 +1,24 @@
 const bgWrapper = document.querySelector('.bg-wrapper');
 const bgWrapperText = document.querySelector('.bg-wrapper__text');
+const cover = document.querySelectorAll('.js-cover');
+const modal = document.querySelectorAll('.js-modal');
+const modalBtn = document.querySelectorAll('.js-modal-btn');
+console.log(modalBtn);
 
 let last_known_scroll_position = 0;
 let ticking = false;
+
+const modalObj = {
+  showModal0: () => modal[0].classList.add('show-modal'),
+  hiddenModal0: () => modal[0].classList.remove('show-modal'),
+};
+
+const Research = () => {
+  cover[0].addEventListener('click', modalObj.showModal0);
+  modalBtn[0].addEventListener('click', modalObj.hiddenModal0);
+};
+
+Research();
 
 const bgText = (scroll_pos) => {
   if (scroll_pos >= 1) {
