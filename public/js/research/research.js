5e434e6c970b3d50b21e58c3136940b506142564
ppby <salswnss@gmail.com>
2020-06-17 14:41:04 +0900

    Add research media 768px

--- public/js/research/research.js
+++ public/js/research/research.js
@@ -7,6 +7,34 @@ const modalBtn = document.querySelectorAll('.js-modal-btn');
 let last_known_scroll_position = 0;
 let ticking = false;
 
+const madalObj = {
+  showModal0: () => {
+    modal[0].classList.remove('hidden-modal');
+    modal[0].classList.add('show-modal');
+  },
+  showModal1: () => {
+    modal[1].classList.remove('hidden-modal');
+    modal[1].classList.add('show-modal');
+  },
+  showModal2: () => {
+    modal[2].classList.remove('hidden-modal');
+    modal[2].classList.add('show-modal');
+  },
+
+  hiddenModal0: () => {
+    modal[0].classList.remove('show-modal');
+    modal[0].classList.add('hidden-modal');
+  },
+  hiddenModal1: () => {
+    modal[1].classList.remove('show-modal');
+    modal[1].classList.add('hidden-modal');
+  },
+  hiddenModal2: () => {
+    modal[2].classList.remove('show-modal');
+    modal[2].classList.add('hidden-modal');
+  },
+};
+
 const bgText = (scroll_pos) => {
   if (scroll_pos >= 1) {
     bgWrapper.classList.remove('animationOut');
@@ -21,32 +49,52 @@ const bgText = (scroll_pos) => {
   }
 };
 
-cover[0].onclick = () => {
-  modal[0].classList.remove('hidden-modal');
-  modal[0].classList.add('show-modal');
-};
-modalBtn[0].onclick = () => {
-  modal[0].classList.remove('show-modal');
-  modal[0].classList.add('hidden-modal');
-};
+const modalEvent = (valid) => {
+  if ('ontouchstart' in document.documentElement === true) {
+    cover[0].ontouchend = () => {
+      madalObj.showModal0();
+    };
+    modalBtn[0].ontouchend = () => {
+      madalObj.hiddenModal0();
+    };
 
-cover[1].onclick = () => {
-  modal[1].classList.remove('hidden-modal');
-  modal[1].classList.add('show-modal');
-};
-modalBtn[1].onclick = () => {
-  modal[1].classList.remove('show-modal');
-  modal[1].classList.add('hidden-modal');
-};
+    cover[1].ontouchend = () => {
+      madalObj.showModal1();
+    };
+    modalBtn[1].ontouchend = () => {
+      madalObj.hiddenModal1();
+    };
 
-cover[2].onclick = () => {
-  modal[2].classList.remove('hidden-modal');
-  modal[2].classList.add('show-modal');
+    cover[2].ontouchend = () => {
+      madalObj.showModal2();
+    };
+    modalBtn[2].ontouchend = () => {
+      madalObj.hiddenModal2();
+    };
+  } else {
+    cover[0].onclick = () => {
+      madalObj.showModal0();
+    };
+    modalBtn[0].onclick = () => {
+      madalObj.hiddenModal0();
+    };
+
+    cover[1].onclick = () => {
+      madalObj.showModal1();
+    };
+    modalBtn[1].onclick = () => {
+      madalObj.hiddenModal1();
+    };
+
+    cover[2].onclick = () => {
+      madalObj.showModal2();
+    };
+    modalBtn[2].onclick = () => {
+      madalObj.hiddenModal2();
+    };
+  }
 };
-modalBtn[2].onclick = () => {
-  modal[2].classList.remove('show-modal');
-  modal[2].classList.add('hidden-modal');
-};
+modalEvent();
 
 window.addEventListener('scroll', () => {
   last_known_scroll_position = window.scrollY;
