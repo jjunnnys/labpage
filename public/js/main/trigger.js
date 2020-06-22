076760472abfa497ea43399790d3ded37bb75fd1
ppby <salswnss@gmail.com>
2020-06-22 20:42:22 +0900

    Update main page up btn

--- public/js/main/trigger.js
+++ public/js/main/trigger.js
@@ -1,4 +1,5 @@
 const trigger = document.querySelector('.js-trigger');
+const pageUp = document.querySelector('.js-page-up');
 
 const triggerHandle = () => {
   window.scrollTo({
@@ -8,8 +9,17 @@ const triggerHandle = () => {
   });
 };
 
+const pageUpHandle = () => {
+  window.scrollTo({
+    top: 0,
+    left: 100,
+    behavior: 'smooth',
+  });
+};
+
 const Trigger = () => {
   trigger.addEventListener('click', triggerHandle);
+  pageUp.addEventListener('click', pageUpHandle);
 };
 
 Trigger();
