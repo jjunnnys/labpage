0e5cdd2ce4c306a39375d6f97e74a36423d0e550
ppby <salswnss@gmail.com>
2020-06-24 16:18:29 +0900

    Add tutorial router

--- controllers/index.js
+++ controllers/index.js
@@ -14,6 +14,7 @@ router.get('/', async (req, res) => {
   });
 });
 
+router.use('/tutorial', require('./tutorial'));
 router.use('/contact', require('./contact'));
 router.use('/digitalTwin', require('./digitalTwin'));
 router.use('/gallery', require('./gallery'));
